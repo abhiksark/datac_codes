22404893004a62f65d33ec96b07ba9e7402929e1
Abhik <abhiksark@gmail.com>
2018-01-01 11:27:28 +0530

    Added First Scripts

---
+++ Introduction to Databases in Python/Engines and Connection Strings.py
@@ -0,0 +1,7 @@
+from sqlalchemy import create_engine 
+
+# Create an engine that connects to the census.sqlite file: engine
+engine = create_engine('sqlite:///census.sqlite')
+
+# Print table names
+print(engine.table_names())
